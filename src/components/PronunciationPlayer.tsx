136a47e5db3da8c419542766af0e8b087719f53e
Martins Aloba <aloba.martins@gmail.com>
2025-06-05 00:36:29 -0500

    Refactor PronunciationPlayer component for consistent SVG class ordering and improved error message styling

--- src/components/PronunciationPlayer.tsx
+++ src/components/PronunciationPlayer.tsx
@@ -66,7 +66,7 @@ const PronunciationPlayer: React.FC<PronunciationPlayerProps> = ({
       >
         {isPlaying ? (
           <span className="playing-indicator">
-            <svg className="animate-spin -ml-1 mr-2 h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <svg className="w-4 h-4 mr-2 -ml-1 text-white animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
               <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
               <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
             </svg>
@@ -74,7 +74,7 @@ const PronunciationPlayer: React.FC<PronunciationPlayerProps> = ({
           </span>
         ) : (
           <span className="play-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" />
             </svg>
             Listen
@@ -82,7 +82,7 @@ const PronunciationPlayer: React.FC<PronunciationPlayerProps> = ({
         )}
       </button>
       
-      {error && <div className="error-message text-red-500 text-sm mt-1">{error}</div>}
+      {error && <div className="mt-1 text-sm text-red-500 error-message">{error}</div>}
     </div>
   );
 };
